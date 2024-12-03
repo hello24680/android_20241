4e40235db0bb9d367c716b769a3f750e9cd1b707
hello24680 <bahoang19042004@gmail.com>
2024-12-03 23:22:29 +0700

    StudentManagerfragment

---
+++ btvn_27_11/StudentManager_Fragment/app/src/main/java/com/example/studentmanagement/database/StudentData.kt
@@ -0,0 +1,28 @@
+package com.example.studentmanagement.database
+
+import com.example.studentmanagement.model.StudentModel
+object StudentData {
+    val students = mutableListOf(
+        StudentModel("Nguyễn Thị Linh", "SV001"),
+        StudentModel("Lê Thị Ngọc", "SV002"),
+        StudentModel("Lê Hoàng Cường", "SV003"),
+        StudentModel("Phạm Thị Dung", "SV004"),
+        StudentModel("Đỗ Minh Đức", "SV005"),
+        StudentModel("Vũ Thị Hoa", "SV006"),
+        StudentModel("Hoàng Văn Hải", "SV007"),
+        StudentModel("Bùi Thị Hạnh", "SV008"),
+        StudentModel("Đinh Văn Hùng", "SV009"),
+        StudentModel("Nguyễn Văn An", "SV010"),
+        StudentModel("Phạm Văn Long", "SV011"),
+        StudentModel("Trần Thị Mai", "SV012"),
+        StudentModel("Trần Thị Bảo", "SV013"),
+        StudentModel("Vũ Văn Nam", "SV014"),
+        StudentModel("Hoàng Thị MU", "SV015"),
+        StudentModel("Đỗ Văn Quân", "SV016"),
+        StudentModel("Nguyễn Thị Thu", "SV017"),
+        StudentModel("Trần Văn Tài", "SV018"),
+        StudentModel("Phạm Thị Tuyết", "SV019"),
+        StudentModel("Lê Văn Vũ", "SV020")
+
+    )
+}
